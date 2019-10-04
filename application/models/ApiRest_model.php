15f0e9aa9727fba78b5228191b5165ddb2e38839
rodrigosecko <rodrigosecko@gmail.com>
2019-10-04 16:53:18 -0400

    api rest cambio a query builder

--- application/models/ApiRest_model.php
+++ application/models/ApiRest_model.php
@@ -8,18 +8,23 @@ class ApiRest_model extends CI_Model {
     } 
       
     function getData() {//obtiene los datos de la tabla tipo_predio en array result
-        $query = $this->db->query('SELECT inicio as texto, persona_id as fecha, asignacion_id as icon, tramite_id as ruta  FROM inspeccion.asignacion where activo=1 ');
+         $this->db->select('inicio as texto, persona_id as fecha, asignacion_id as icon, tramite_id as ruta');
+        $query = $this->db->get_where('inspeccion.asignacion',array('activo' => 1 ));
         return $query->result_array();
     }
 
     function getGrupos() {//obtiene los datos de la tabla tipo_predio en array result
-        $query = $this->db->query('SELECT grupo_mat_id as id_g,descripcion as texto FROM catastro.bloque_grupo_mat WHERE activo=1');
+        
+        $this->db->select('grupo_mat_id as id_g,descripcion as texto');
+        $query = $this->db->get_where('catastro.bloque_grupo_mat',array('activo' => 1 ));
+        
         return $query->result_array();
     }
 
     function getSubgrupos($id) {//obtiene los datos de la tabla tipo_predio en array result
-    	
-        $query = $this->db->query("SELECT grupo_mat_id,descripcion as texto FROM catastro.bloque_mat_item WHERE activo=1 and grupo_mat_id=$id");
+        $this->db->select('grupo_mat_id, descripcion as texto');
+    	$query= $this->db->get_where('catastro.bloque_mat_item',array('activo' =>1 ,'grupo_mat_id' => $id));
+       
         return $query->result_array();
     }
     function getlistadotramite() {//obtiene los datos de la tabla tipo_predio en array result
@@ -27,8 +32,15 @@ class ApiRest_model extends CI_Model {
         return $query->result_array();
     }
      function getRequisitos($id) {//obtiene los datos de la tabla tipo_predio en array result
+        $this->db->select('descripcion');
+        $query = $this->db->get_where('tramite.requisito', array('activo' =>1 ,'tipo_tramite_id'=>$id ));        
+        return $query->result_array();
+    }
+       function getReq($id) {//obtiene los datos de la tabla tipo_predio en array result
         
-        $query = $this->db->query("SELECT descripcion FROM tramite.requisito where activo=1 and tipo_tramite_id=$id");
+        $this->db->select('descripcion');
+        //$this->db->select('descripcion as desc');
+        $query = $this->db->get_where('tramite.requisito',array('activo' => 1, 'tipo_tramite_id'=>$id));
         return $query->result_array();
     }
 }
