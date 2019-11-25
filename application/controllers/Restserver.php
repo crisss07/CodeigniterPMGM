92e0982aa4f8396052272653142d1d15fa96823c
rodrigosecko <rodrigosecko@gmail.com>
2019-11-25 18:24:45 -0400

    api get login mobile app

--- application/controllers/Restserver.php
+++ application/controllers/Restserver.php
@@ -36,11 +36,12 @@ class Restserver extends CI_Controller{
     }
 
     public function asinglist_get(){
+        $id = $this->get('id');
         $this->load->model("ApiRest_model");
         //$array = array("Hola","Mundo","Codeigniter");
         //$this->response($this->Edificacion_model->get_Bloque());
         //$this->response($array);       
-        $user = array('respuesta' => $this->ApiRest_model->get_asign_list(63));
+        $user = array('respuesta' => $this->ApiRest_model->get_asign_list($id));
         if($user)
         {
             $this->response( $user, 200); // 200 being the HTTP response code
@@ -161,10 +162,62 @@ class Restserver extends CI_Controller{
         }
         else{
             $this->response($message, 404);
-        }
+        }                      
+    }
+
+    public function derivacionst_get(){
+        $id = $this->get('id');
+        $token = $this->get('token');
+        $this->load->model("ApiRest_model");
+ 
+        $message = array('mensaje' => 'Acceso denegado');
+        $datos_nulos = array('estado' => 'asd','mensaje'=>'No se encontraron datos');
+       
+            $user = array('estado' => $this->ApiRest_model->derivacion($id),'mensaje'=>'Datos encontrados ','bool'=>'TRUE');
+            if(!$this->ApiRest_model->derivacion($id)){
+                $user = array('estado' => $this->ApiRest_model->derivacion($id),'mensaje'=>'No se encontraron datos','bool'=>'FALSE');
+            }
+            if($user)
+            {
+                $this->response( $user, 200); // 200 being the HTTP response code
+            } 
+            else
+            {
+               
+                $this->response($message, 404);
+            }
+
+       
               
         
     }
+
+
+    public function logueo_get()
+    {
+        $username = $this->get('username');
+        $password = $this->get('password');  
+
+      
+        //$username = 'adm';
+        //$password = 'adm';
+        $this->load->model("ApiRest_model");
+        $user = array('respuesta' => $this->ApiRest_model->data_login($username,md5($password)),'mensaje'=>'Datos encontrados ','bool'=>'TRUE','usuario'=>$username,'password'=>$password);
+        if(!$this->ApiRest_model->data_login($username,md5($password))){
+                $user = array('respuesta' =>  $this->ApiRest_model->data_login($username,md5($password)),'mensaje'=>'No se encontraron datos','bool'=>'FALSE','password'=>$password,'usuario'=>$username);
+            }
+         
+        if($user)
+        {
+            $this->response( $user, 200); // 200 being the HTTP response code
+        } 
+        else
+        {
+            $this->response(NULL, 404);
+        }
+    }
+
+
     
 }
      
