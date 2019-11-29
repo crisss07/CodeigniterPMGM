5ccf46106756c0b3de01a92b9a9e0b036f82cd6a
Cristiam Herrera <cristiamherrera@gmail.com>
2019-11-29 11:18:33 -0400

    Se adiciono un archivo databases.sample

--- application/views/predios/guarda.php
+++
@@ -1,6 +0,0 @@
-<?php foreach ($fotos as $f): ?>
-    <?php //echo pg_unescape_bytea($f->foto_plano_ubi); ?>
-    <?php echo $f->codcatas; ?>
-    <img src="<?php //echo pg_unescape_bytea($f->foto_plano_ubi); ?>" alt="Smiley face" height="42" width="42">
-    <br />
-<?php endforeach; ?>
